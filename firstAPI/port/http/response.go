57b24185421b52965593a4a094292e392f34bbaa
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-09 14:35:35 +0330

    add: response for http port

--- firstAPI/port/http/response.go
+++ firstAPI/port/http/response.go
@@ -1,1 +1,12 @@
 package http
+
+// Response
+// is the response data of get handler.
+type Response struct {
+	Id          uint64 `json:"id"`
+	Description string `json:"description"`
+	Email       string `json:"email"`
+	State       string `json:"state"`
+	Category    string `json:"category"`
+	Image       string `json:"image"`
+}
