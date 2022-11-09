e9bb0477ba9ddd4943913a9e52485806f6ccfd43
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-09 14:41:01 +0330

    add: mongodb to http handler

--- firstAPI/port/http/handler.go
+++ firstAPI/port/http/handler.go
@@ -1,10 +1,14 @@
 package http
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 // Handler
 // manages to handle http endpoints.
 type Handler struct {
+	Mongo *mongo.Database
 }
 
 // HandleGetRequests
