a6f11ad4e73a14463c3adea3a6b268e747bf594f
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-09 15:57:56 +0330

    add: api setup

--- internal/api/cmd/root.go
+++ internal/api/cmd/root.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"github.com/ceit-aut/ad-registration-service/internal/api/http"
+	"github.com/ceit-aut/ad-registration-service/pkg/config"
+	"github.com/ceit-aut/ad-registration-service/pkg/mqtt"
+	"github.com/ceit-aut/ad-registration-service/pkg/storage/mongodb"
+	s32 "github.com/ceit-aut/ad-registration-service/pkg/storage/s3"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
 )
@@ -17,8 +22,40 @@ func GetCommand() *cobra.Command {
 
 // main method of api service.
 func main() {
+	// load configs
+	cfg := config.Load()
+
+	// mongodb connection
+	mongo, err := mongodb.NewConnection(cfg.Storage.Mongo)
+	if err != nil {
+		panic(err)
+	}
+
+	// s3 connection
+	s3, err := s32.NewSession(cfg.Storage.S3)
+	if err != nil {
+		panic(err)
+	}
+
+	// rabbitmq connection
+	mq, err := mqtt.NewConnection(cfg.MQTT)
+	if err != nil {
+		panic(err)
+	}
+
+	// creating a handler
+	h := http.Handler{
+		Mongo: mongo,
+		MQTT:  mq,
+		S3:    s3,
+	}
+
 	// creating a new fiber app
 	app := fiber.New()
+
+	// declaring endpoints
+	app.Get("/{id}", h.HandleGetRequests)
+	app.Post("/", h.HandlePostRequests)
 
 	// starting fiber
 	if err := app.Listen(":5050"); err != nil {
