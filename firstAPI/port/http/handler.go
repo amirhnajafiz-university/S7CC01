a7e0c4d4b0338be220b94993add7f910fa9bf686
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-09 14:31:20 +0330

    add: handler struct

--- firstAPI/port/http/handler.go
+++ firstAPI/port/http/handler.go
@@ -1,1 +1,21 @@
 package http
+
+import "github.com/gofiber/fiber/v2"
+
+// Handler
+// manages to handle http endpoints.
+type Handler struct {
+}
+
+// HandleGetRequests
+// return a json response of user ad.
+func (h *Handler) HandleGetRequests(ctx *fiber.Ctx) {
+
+}
+
+// HandlePostRequests
+// get ad request and save it into mongodb and s3.
+// after that send the id over rabbitMQ.
+func (h *Handler) HandlePostRequests(ctx *fiber.Ctx) {
+
+}
