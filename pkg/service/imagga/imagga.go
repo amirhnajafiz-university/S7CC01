457554d0111491e49e74f5141b8f6b0e277fb750
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-09 17:40:14 +0330

    add: response in process call of imagga

--- pkg/service/imagga/imagga.go
+++ pkg/service/imagga/imagga.go
@@ -1,6 +1,8 @@
 package imagga
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -13,19 +15,24 @@ type Imagga struct {
 
 // Process
 // sends one http request to Imagga website.
-func (i *Imagga) Process(url string) (string, error) {
+func (i *Imagga) Process(url string) (*Response, error) {
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("GET", "https://api.imagga.com/v2/tags?image_url="+url, nil)
 	req.SetBasicAuth(i.Cfg.ApiKey, i.Cfg.ApiSecret)
 
 	resp, err := client.Do(req)
-
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	respBody, _ := ioutil.ReadAll(resp.Body)
 
-	return string(respBody), nil
+	var response Response
+
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	return &response, nil
 }
